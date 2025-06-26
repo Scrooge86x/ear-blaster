928dd3bf0f838e7ae41c5160a24315276b5e5778
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-06-26 17:56:27 +0200

    Replaced QStringLiteral macro with _s string literal.

--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QGuiApplication app{ argc, argv };
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/ear-blaster/qml/main.qml")));
+    QQmlApplicationEngine engine{};
+
+    using namespace Qt::Literals::StringLiterals;
+    engine.load(QUrl(u"qrc:/qt/qml/ear-blaster/qml/main.qml"_s));
     if (engine.rootObjects().isEmpty())
         return -1;
 
