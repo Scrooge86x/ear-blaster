35f77db9eb826f430516c4d49404bb4571a19494
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-07-07 06:38:12 +0200

    Reduced the amount of ffmpeg logging noise.

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QIcon>
+#include <QLoggingCategory>
 
 #include "globalkeylistener.h"
 #include "translator.h"
@@ -12,11 +13,13 @@ int main(int argc, char *argv[])
     // Fix for resize causing flickering
     qputenv("QT_D3D_NO_FLIP", "1" );
 
+    using namespace Qt::Literals::StringLiterals;
+    QLoggingCategory::setFilterRules(u"qt.multimedia.ffmpeg.*=false"_s);
+
     // Qt documentation says that QApplication should be used instead
     // because of Qt Labs Platform, but this would disable the dark titlebar
     // on windows 10 so for now it will stay as QGuiApplication
     QGuiApplication app{ argc, argv };
-    using namespace Qt::Literals::StringLiterals;
 
     // The icon already set from the windows rc file, but in order for
     // ColorDialog to pick it up, it needs to also be set here
