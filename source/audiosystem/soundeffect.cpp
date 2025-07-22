Scrooge86x/ear-blaster

commit 4106b5c72d1e8542cac908500b62f60d33466efa

Fixed microphone always being enabled by default. Added suspending of audio sink when it's not needed in MicrophonePassthrough. Moved constructor initialization before creating a new thread in SoundEffect and MicrophonePassthrough.

source/audiosystem/soundeffect.cpp
@@ -17,47 +17,46 @@ SoundEffect::SoundEffect(
     , m_outputAudioDevice{ outputAudioDevice }
     , m_monitorAudioDevice{ monitorAudioDevice }
 {
-    connect(&m_thread, &QThread::started, this, [this] {
-        m_decoder = new QAudioDecoder{ this };
-        m_decoder->setAudioFormat(AudioShared::getAudioFormat());
+    m_decoder = new QAudioDecoder{ this };
+    m_decoder->setAudioFormat(AudioShared::getAudioFormat());
 
-        connect(this, &SoundEffect::stopRequested,
-                this, &SoundEffect::stop);
+    connect(this, &SoundEffect::stopRequested,
+            this, &SoundEffect::stop);
 
-        connect(&m_outputAudioDevice, &AudioDevice::enabledChanged, this, [this](const bool enabled) {
-            if (!enabled) {
-                stop();
-            }
-        });
-        connect(&m_monitorAudioDevice, &AudioDevice::enabledChanged, this, [this](const bool enabled) {
-            if (!m_monitorAudioSink) {
-                return;
-            }
-
-            if (enabled) {
-                m_monitorIODevice = m_monitorAudioSink->start();
-            } else {
-                m_monitorAudioSink->reset();
-            }
-        });
-
-        const auto mediaDevices{ new QMediaDevices{ this } };
-        connect(mediaDevices, &QMediaDevices::audioOutputsChanged, this, [this] {
-            if (!QMediaDevices::audioOutputs().contains(m_outputAudioDevice.device())) {
-                invalidateAudioOutputSink();
-            }
-            if (!QMediaDevices::audioOutputs().contains(m_monitorAudioDevice.device())) {
-                invalidateAudioMonitorSink();
-            }
-        });
-
-        connect(&m_outputAudioDevice, &AudioDevice::deviceChanged,
-                this, &SoundEffect::initAudioOutputSink);
-        connect(&m_monitorAudioDevice, &AudioDevice::deviceChanged,
-                this, &SoundEffect::initAudioMonitorSink);
-        initAudioOutputSink();
-        initAudioMonitorSink();
+    connect(&m_outputAudioDevice, &AudioDevice::enabledChanged, this, [this](const bool enabled) {
+        if (!enabled) {
+            stop();
+        }
     });
+    connect(&m_monitorAudioDevice, &AudioDevice::enabledChanged, this, [this](const bool enabled) {
+        if (!m_monitorAudioSink) {
+            return;
+        }
+
+        if (enabled) {
+            m_monitorIODevice = m_monitorAudioSink->start();
+        } else {
+            m_monitorAudioSink->reset();
+        }
+    });
+
+    const auto mediaDevices{ new QMediaDevices{ this } };
+    connect(mediaDevices, &QMediaDevices::audioOutputsChanged, this, [this] {
+        if (!QMediaDevices::audioOutputs().contains(m_outputAudioDevice.device())) {
+            invalidateAudioOutputSink();
+        }
+        if (!QMediaDevices::audioOutputs().contains(m_monitorAudioDevice.device())) {
+            invalidateAudioMonitorSink();
+        }
+    });
+
+    connect(&m_outputAudioDevice, &AudioDevice::deviceChanged,
+            this, &SoundEffect::initAudioOutputSink);
+    connect(&m_monitorAudioDevice, &AudioDevice::deviceChanged,
+            this, &SoundEffect::initAudioMonitorSink);
+    initAudioOutputSink();
+    initAudioMonitorSink();
+
     m_thread.start();
     this->moveToThread(&m_thread);
 }

source/audiosystem/audiosystem.cpp
@@ -16,6 +16,7 @@ AudioSystem::AudioSystem(QObject *const parent)
         m_micPassthrough->outputDevice()->setDevice(m_outputAudioDevice->device());
     });
 
+    m_micPassthrough->inputDevice()->setEnabled(false);
     m_monitorAudioDevice->setEnabled(false);
 
     const auto mediaDevices{ new QMediaDevices{ this } };

source/audiosystem/microphonepassthrough.cpp
@@ -12,30 +12,29 @@
 MicrophonePassthrough::MicrophonePassthrough()
     : QObject{ nullptr }
 {
-    connect(&m_thread, &QThread::started, this, [this] {
-        m_inputAudioDevice = new AudioDevice{ this };
-        connect(m_inputAudioDevice, &AudioDevice::deviceChanged,
-                this, &MicrophonePassthrough::initAudioSource);
-        connect(m_inputAudioDevice, &AudioDevice::enabledChanged,
-                this, &MicrophonePassthrough::onInputEnabledChanged);
+    m_inputAudioDevice = new AudioDevice{ this };
+    connect(m_inputAudioDevice, &AudioDevice::deviceChanged,
+            this, &MicrophonePassthrough::initAudioSource);
+    connect(m_inputAudioDevice, &AudioDevice::enabledChanged,
+            this, &MicrophonePassthrough::onInputEnabledChanged);
 
-        m_outputAudioDevice = new AudioDevice{ this };
-        connect(m_outputAudioDevice, &AudioDevice::deviceChanged,
-                this, &MicrophonePassthrough::initAudioSink);
-        // Output device is always enabled as of now
+    m_outputAudioDevice = new AudioDevice{ this };
+    connect(m_outputAudioDevice, &AudioDevice::deviceChanged,
+            this, &MicrophonePassthrough::initAudioSink);
+    // Output device is suspended in onInputEnabledChanged
 
-        const auto mediaDevices{ new QMediaDevices{ this } };
-        connect(mediaDevices, &QMediaDevices::audioOutputsChanged, this, [this] {
-            if (!QMediaDevices::audioOutputs().contains(m_outputAudioDevice->device())) {
-                invalidateAudioSink();
-            }
-        });
-        connect(mediaDevices, &QMediaDevices::audioInputsChanged, this, [this] {
-            if (!QMediaDevices::audioInputs().contains(m_inputAudioDevice->device())) {
-                invalidateAudioSource();
-            }
-        });
+    const auto mediaDevices{ new QMediaDevices{ this } };
+    connect(mediaDevices, &QMediaDevices::audioOutputsChanged, this, [this] {
+        if (!QMediaDevices::audioOutputs().contains(m_outputAudioDevice->device())) {
+            invalidateAudioSink();
+        }
     });
+    connect(mediaDevices, &QMediaDevices::audioInputsChanged, this, [this] {
+        if (!QMediaDevices::audioInputs().contains(m_inputAudioDevice->device())) {
+            invalidateAudioSource();
+        }
+    });
+
     m_thread.start();
     this->moveToThread(&m_thread);
 }
@@ -59,12 +58,14 @@ void MicrophonePassthrough::onInputEnabledChanged(const bool enabled)
     }
 
     if (!enabled) {
+        m_audioSink->suspend();
         return m_audioSource->suspend();
     }
 
     // Calling resume on audio source that is not suspended
     // causes IAudioClient3::Start failed "AUDCLNT_E_NOT_STOPPED"
     if (m_audioSource->state() == QtAudio::SuspendedState) {
+        m_audioSink->resume();
         m_audioSource->resume();
     }
 }
@@ -110,6 +111,9 @@ void MicrophonePassthrough::initAudioSink()
     connect(m_outputAudioDevice, &AudioDevice::volumeChanged,
             m_audioSink, &QAudioSink::setVolume);
     m_outputIODevice = m_audioSink->start();
+    if (!m_inputAudioDevice->enabled()) {
+        m_audioSink->suspend();
+    }
 
     // Reject last buffer so QIODevice::readyRead gets emmited again with new data
     if (m_inputIODevice) {

commit bb3b2523a04887ee43faf5cd795f16f90b37002f

Fixed a bug introduced by 4106b5c that caused the audio decoder to work on the main thread which caused stutters when resizing window.

source/audiosystem/soundeffect.cpp
@@ -17,9 +17,6 @@ SoundEffect::SoundEffect(
     , m_outputAudioDevice{ outputAudioDevice }
     , m_monitorAudioDevice{ monitorAudioDevice }
 {
-    m_decoder = new QAudioDecoder{ this };
-    m_decoder->setAudioFormat(AudioShared::getAudioFormat());
-
     connect(this, &SoundEffect::stopRequested,
             this, &SoundEffect::stop);
 
@@ -57,6 +54,9 @@ SoundEffect::SoundEffect(
             this, &SoundEffect::initAudioMonitorSink);
 
     connect(&m_thread, &QThread::started, this, [this] {
+        m_decoder = new QAudioDecoder{ this };
+        m_decoder->setAudioFormat(AudioShared::getAudioFormat());
+
         initAudioOutputSink();
         initAudioMonitorSink();
     });