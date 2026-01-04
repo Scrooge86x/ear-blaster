Scrooge86x/ear-blaster

commit 1a54f9e2c2936b9a5f40c016b6393b8b341ffa88

Add the greatest workaround for format preloading in TextToSpeech

cpp/audiosystem/texttospeech.cpp
@@ -35,12 +35,36 @@ TextToSpeech::TextToSpeech(
         }
     });
 
-    // TODO: Actually preload the right format, this is a placeholder
-    QAudioFormat format{};
-    format.setChannelCount(2);
-    format.setSampleFormat(QAudioFormat::Int16);
-    format.setSampleRate(16'000);
-    m_audioOutput.setFormat(format);
+    connect(&m_thread, &QThread::started, this, [this] {
+        // HACK
+        // the call to QTextToSpeech::synthesize causes the playback to be
+        // laggy for the whole runtime (somehow lags the engine used by the backend?)
+        // the only solutions I've found after trying everything was to call QTextToSpeech::synthesize
+        // after around 3 seconds, but I don't like magic numbers that could be different on another pc
+        // and the other solution is what you see below...
+        // undocumented backend magic like always with less popular features in Qt
+        m_tts.setEngine("mock");
+        m_tts.setEngine("sapi");
+        // HACK END
+
+        // Preload the right format so there is no delay on the first playback
+        using namespace Qt::Literals::StringLiterals;
+        m_tts.synthesize(u"a"_s, this, [this, isInitialized = false](const QAudioBuffer& audioBuffer) mutable {
+            if (isInitialized) {
+                return;
+            }
+            isInitialized = true;
+
+            auto format{ audioBuffer.format() };
+            if (format.channelCount() == 1) {
+                format.setChannelCount(2);
+            }
+
+            QMetaObject::invokeMethod(this, [this, format]() {
+                m_audioOutput.setFormat(format);
+            }, Qt::QueuedConnection);
+        });
+    });
 
     m_thread.start();
     this->moveToThread(&m_thread);
@@ -98,6 +122,8 @@ void TextToSpeech::processQueue()
             format.setChannelCount(2);
         }
 
+        // According to https://doc.qt.io/qt-6/qtexttospeech.html#synthesize
+        // the format can technically change so it must be handled unfortunately
         if (m_audioOutput.format() != format) {
             m_audioOutput.setFormat(format);
             if (!m_audioOutput.start()) {

commit 2a7522aab0da78762d9f0e9240bb038ca941879e

Fix a race condition of tts engine initialization and AppSettings that was introduced by 1a54f9e and caused the wrong locale and voice to be set

cpp/audiosystem/microphonepassthrough.cpp
@@ -36,8 +36,8 @@ MicrophonePassthrough::MicrophonePassthrough(const AudioDevice& outputAudioDevic
         }
     });
 
-    m_thread.start();
     this->moveToThread(&m_thread);
+    m_thread.start();
 }
 
 MicrophonePassthrough::~MicrophonePassthrough()

cpp/audiosystem/soundeffect.cpp
@@ -38,8 +38,9 @@ SoundEffect::SoundEffect(
         m_audioOutput.initialize();
         m_monitorOutput.initialize();
     });
-    m_thread.start();
+
     this->moveToThread(&m_thread);
+    m_thread.start();
 }
 
 SoundEffect::~SoundEffect()

cpp/audiosystem/texttospeech.cpp
@@ -52,6 +52,7 @@ TextToSpeech::TextToSpeech(
         // undocumented backend magic like always with less popular features in Qt
         m_tts.setEngine("mock");
         m_tts.setEngine("sapi");
+        emit engineInitialized();
         // HACK END
 
         // Preload the right format so there is no delay on the first playback
@@ -74,8 +75,14 @@ TextToSpeech::TextToSpeech(
         });
     });
 
-    m_thread.start();
     this->moveToThread(&m_thread);
+    m_thread.start();
+
+    // Wait for the engine to initialize to prevent a race condition with AppSettings
+    QEventLoop waitingLoop{};
+    connect(this, &TextToSpeech::engineInitialized,
+            &waitingLoop, &QEventLoop::quit);
+    waitingLoop.exec();
 }
 
 TextToSpeech::~TextToSpeech()

cpp/audiosystem/texttospeech.h
@@ -44,6 +44,7 @@ public:
 signals:
     void say(const QString& text);
     void stop();
+    void engineInitialized();
 
 private:
     void onSay(const QString& text);

ui/SettingsScreen.qml
@@ -306,7 +306,7 @@ Item {
                         if (voiceIndex !== -1) {
                             ttsVoice.currentIndex = voiceIndex
                         } else {
-                            AppSettings.ttsLocale = model[0].name;
+                            AppSettings.ttsVoice = model[0].name;
                             ttsVoice.currentIndex = 0;
                         }
                     }

ui/settings/AppSettings.qml
@@ -84,8 +84,8 @@ Settings {
         }
     }
     onTtsLocaleChanged: {
-        audioSystem.tts.locale = Qt.locale(ttsLocale)
-        ttsLocale = audioSystem.tts.locale.name
+        audioSystem.tts.locale = Qt.locale(ttsLocale);
+        ttsLocale = audioSystem.tts.locale.name;
     }
     onTtsVoiceChanged: {
         for (const voice of audioSystem.tts.availableVoices()) {