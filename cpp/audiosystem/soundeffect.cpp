Scrooge86x/ear-blaster

commit e14d9742eaed5b6ad6bb84fba4cc2922d40a82ed

Change AudioShared::addOverdrive function to take in QAudioFormat::SampleFormat during runtime instead of compile time

cpp/audiosystem/soundeffect.cpp
@@ -148,15 +148,23 @@ void SoundEffect::processBuffer()
 
         QByteArray samplesCopy{ m_currentBuffer.data<char>() + m_bytesWritten, bytesToWrite };
         if (m_monitorAudioDevice.overdrive()) {
-            auto currentSamples{ reinterpret_cast<AudioShared::SampleType*>(samplesCopy.data()) };
-            AudioShared::addOverdrive(currentSamples, numSamples, m_monitorAudioDevice.overdrive());
+            AudioShared::addOverdrive(
+                samplesCopy.data(),
+                AudioShared::getAudioFormat().sampleFormat(),
+                numSamples,
+                m_monitorAudioDevice.overdrive()
+            );
         }
         monitorIODevice->write(samplesCopy);
     }
 
     if (m_outputAudioDevice.overdrive()) {
-        const auto currentSamples{ m_currentBuffer.data<AudioShared::SampleType>() + samplesWritten };
-        AudioShared::addOverdrive(currentSamples, numSamples, m_outputAudioDevice.overdrive());
+        AudioShared::addOverdrive(
+            m_currentBuffer.data<char>() + samplesWritten,
+            AudioShared::getAudioFormat().sampleFormat(),
+            numSamples,
+            m_outputAudioDevice.overdrive()
+        );
     }
     m_bytesWritten += outputIODevice->write(m_currentBuffer.data<char>() + m_bytesWritten, bytesToWrite);
 

cpp/audiosystem/audioshared.h
@@ -15,29 +15,41 @@ consteval QAudioFormat getAudioFormat() {
     return format;
 }
 
-template <QAudioFormat::SampleFormat> struct SampleFormatType { using type = void; };
-template <> struct SampleFormatType<QAudioFormat::UInt8> { using type = quint8; };
-template <> struct SampleFormatType<QAudioFormat::Int16> { using type = qint16; };
-template <> struct SampleFormatType<QAudioFormat::Int32> { using type = qint32; };
-template <> struct SampleFormatType<QAudioFormat::Float> { using type = float; };
-
-using SampleType = SampleFormatType<getAudioFormat().sampleFormat()>::type;
-
-template <typename T>
-void addOverdrive(
-    T* samples,
+inline void addOverdrive(
+    char* samples,
+    const QAudioFormat::SampleFormat sampleFormat,
     const qint64 numSamples,
     const float boostLevel
 ) {
-    constexpr auto minVal{ std::numeric_limits<SampleType>::min() };
-    constexpr auto maxVal{ std::numeric_limits<SampleType>::max() };
-    const double gain{ 1.0 + QtAudio::convertVolume(
-        boostLevel, QtAudio::LogarithmicVolumeScale, QtAudio::LinearVolumeScale
-    ) * 150.0 };
+    const auto applyOverdrive = [&samples, &numSamples, &boostLevel]<typename T>() {
+        constexpr auto minVal{ std::numeric_limits<T>::min() };
+        constexpr auto maxVal{ std::numeric_limits<T>::max() };
+        const double gain{ 1.0 + QtAudio::convertVolume(
+            boostLevel, QtAudio::LogarithmicVolumeScale, QtAudio::LinearVolumeScale
+        ) * 150.0 };
 
-    for (qint64 i{}; i < numSamples; ++i) {
-        double boosted{ static_cast<double>(*samples) * gain };
-        *samples++ = std::clamp<double>(boosted, minVal, maxVal);
+        T* tSamples{ reinterpret_cast<T*>(samples) };
+        for (qint64 i{}; i < numSamples; ++i) {
+            double boosted{ static_cast<double>(*tSamples) * gain };
+            *tSamples++ = std::clamp<double>(boosted, minVal, maxVal);
+        }
+    };
+
+    switch (sampleFormat) {
+    case QAudioFormat::UInt8:
+        applyOverdrive.template operator()<quint8>();
+        break;
+    case QAudioFormat::Int16:
+        applyOverdrive.template operator()<qint16>();
+        break;
+    case QAudioFormat::Int32:
+        applyOverdrive.template operator()<qint32>();
+        break;
+    case QAudioFormat::Float:
+        applyOverdrive.template operator()<float>();
+        break;
+    default:
+        break;
     }
 }
 
@@ -68,20 +80,20 @@ inline QAudioBuffer monoToStereo(const QAudioBuffer& monoBuffer)
     };
 
     switch (monoBufferFormat.sampleFormat()) {
-        case QAudioFormat::UInt8:
-            monoBufferToStereoBuffer.template operator()<quint8>();
-            break;
-        case QAudioFormat::Int16:
-            monoBufferToStereoBuffer.template operator()<qint16>();
-            break;
-        case QAudioFormat::Int32:
-            monoBufferToStereoBuffer.template operator()<qint32>();
-            break;
-        case QAudioFormat::Float:
-            monoBufferToStereoBuffer.template operator()<float>();
-            break;
-        default:
-            break;
+    case QAudioFormat::UInt8:
+        monoBufferToStereoBuffer.template operator()<quint8>();
+        break;
+    case QAudioFormat::Int16:
+        monoBufferToStereoBuffer.template operator()<qint16>();
+        break;
+    case QAudioFormat::Int32:
+        monoBufferToStereoBuffer.template operator()<qint32>();
+        break;
+    case QAudioFormat::Float:
+        monoBufferToStereoBuffer.template operator()<float>();
+        break;
+    default:
+        break;
     }
 
     return stereoBuffer;

cpp/audiosystem/microphonepassthrough.cpp
@@ -85,7 +85,8 @@ void MicrophonePassthrough::processBuffer()
 
     QByteArray buffer{ m_inputIODevice->readAll() };
     AudioShared::addOverdrive(
-        reinterpret_cast<AudioShared::SampleType*>(buffer.data()),
+        buffer.data(),
+        AudioShared::getAudioFormat().sampleFormat(),
         buffer.length(),
         m_inputAudioDevice->overdrive()
     );

commit 4ffdb0293bbfbdf510dc36fe53a79deda939c315

Fix a bug introduced by e14d974 that caused the overdrive to not be applied correctly to the output device

cpp/audiosystem/soundeffect.cpp
@@ -130,7 +130,6 @@ void SoundEffect::processBuffer()
     }
 
     constexpr int bytesPerSample{ AudioShared::getAudioFormat().bytesPerSample() };
-    const auto samplesWritten{ m_bytesWritten / bytesPerSample };
     const qint64 bytesToWrite{ std::min(
         outputAudioSink->bytesFree(), m_currentBuffer.byteCount() - m_bytesWritten
     ) };
@@ -160,7 +159,7 @@ void SoundEffect::processBuffer()
 
     if (m_outputAudioDevice.overdrive()) {
         AudioShared::addOverdrive(
-            m_currentBuffer.data<char>() + samplesWritten,
+            m_currentBuffer.data<char>() + m_bytesWritten,
             AudioShared::getAudioFormat().sampleFormat(),
             numSamples,
             m_outputAudioDevice.overdrive()

cpp/audiosystem/texttospeech.cpp
@@ -188,7 +188,6 @@ void TextToSpeech::processQueue()
         return;
     }
 
-    const auto samplesWritten{ m_bytesWritten / bytesPerSample };
     const qint64 bytesToWrite{ std::min(
         outputAudioSink->bytesFree(), m_currentBuffer.byteCount() - m_bytesWritten
     ) };
@@ -218,7 +217,7 @@ void TextToSpeech::processQueue()
 
     if (m_outputAudioDevice.overdrive()) {
         AudioShared::addOverdrive(
-            m_currentBuffer.data<char>() + samplesWritten,
+            m_currentBuffer.data<char>() + m_bytesWritten,
             m_audioOutput.format().sampleFormat(),
             numSamples,
             m_outputAudioDevice.overdrive()